7459d81a3c7ac7669e227b07854a68812a488eea
seung8 <lsy0914@naver.com>
2018-03-03 16:14:22 +0900

    first commit.

---
+++ upload_files.py
@@ -0,0 +1,31 @@
+import timeit
+import boto3
+import requests
+
+def upload_files():
+    # measure execution time
+    start_time = timeit.default_timer()
+
+    # aws auth
+    session = boto3.Session(
+        aws_access_key_id='your access key'
+        aws_secret_access_key='your secret key',
+        region_name='your bucket region',
+    )
+    s3 = session.resource('s3')
+    bucket = 'your s3 bucket'
+
+    # download image file
+    url = 'download.url/{}'.format('download_file_name.jpg')
+    response = requests.get(url, stream=True)
+    response_data = response.raw
+    upload_data = response_data.read()
+
+    # upload image file to s3 bucket
+    # path = default path is bucket name - ex) '/my_bucket/'
+    # if specify a detailed path, add path before {}
+    # for example, '/my_bucket/photos/save_file' -> Key='photos/{}'.format('filename.extension')
+    s3.Bucket(bucket).put_object(Key='{}'.format('filename'), Body=upload_data)
+
+    end_time = timeit.default_timer()
+    print('### total run-time - {}sec'.format(end_time - start_time))
